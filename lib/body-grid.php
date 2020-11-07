6566adb99a5956db08d11d6bfea6d6715e4d06d2
steelbridge-io <chris@steelbridge.io>
2020-11-07 15:56:57 -0800

    Added grid body

--- lib/body-grid.php
+++ lib/body-grid.php
@@ -3,4 +3,17 @@
  * Body grid
  */
 
-
+add_action('genesis_after_content_sidebar_wrap', 'add_body_grid', 5);
+function add_body_grid() {
+	echo '
+				<div id="grid-body">
+				<div class="container-fluid">
+				<div class="row">
+				<div class="title-area text-center">
+				<h3>Grid Body</h3>
+				</div>
+				</div>
+				</div>
+				</div>
+	';
+}
