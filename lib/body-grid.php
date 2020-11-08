c9ac291e8e4ad3bfe3385ec1f679848b179dccf4
steelbridge-io <chris@steelbridge.io>
2020-11-07 16:07:34 -0800

    Added body-grid containers

--- lib/body-grid.php
+++ lib/body-grid.php
@@ -7,13 +7,23 @@ add_action('genesis_after_content_sidebar_wrap', 'add_body_grid', 5);
 function add_body_grid() {
 	echo '
 				<div id="grid-body">
-				<div class="container-fluid">
-				<div class="row">
-				<div class="title-area text-center">
-				<h3>Grid Body</h3>
-				</div>
-				</div>
-				</div>
+					<div class="container-fluid">
+						<div class="row">
+							<div class="title-area text-center">
+							<h3>Grid Body</h3>
+							</div>
+							<div class="container">
+							<div class="row justify-content-center">
+							<div class="col-md-3">#1</div>
+							<div class="col-md-3">#2</div>
+							</div>
+							<div class="row justify-content-center">
+							<div class="col-md-3">#3</div>
+							<div class="col-md-3">#4</div>
+							</div>
+							</div>
+						</div>
+					</div>
 				</div>
 	';
 }
