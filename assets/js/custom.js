5afc4d243103d3d8315f099e0e5488266034f633
steelbridge-io <chris@steelbridge.io>
2020-11-15 18:01:40 -0800

    Added animation on scroll to nav, updated margins and padding

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,10 @@ $('.parallax-window').parallax({
   naturalWidth: 600,
   naturalHeight: 400
 });
+
+$(function () {
+  $(document).scroll(function () {
+	 var $nav = $(".navbar.fixed-top");
+	 $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
